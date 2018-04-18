3ca02e62c34230f6a4ea98d6f0a8a11629ce2260
Yohan Florez <yflorezr@gmil.com>
2018-04-18 18:39:46 -0500

    Query Users

--- application/classes/Controller/Api/Validation.php
+++ application/classes/Controller/Api/Validation.php
@@ -1,34 +1,43 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
 /**
- * Ushahidi API Users Controller
+ * Ushahidi API Index Controller
  *
  * @author     Ushahidi Team <team@ushahidi.com>
  * @package    Ushahidi\Application\Controllers
- * @copyright  2013 Ushahidi
+ * @copyright  2014 Ushahidi
  * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
  */
+
 class Controller_Api_Validation extends Ushahidi_Rest {
+
+	/**
+	 * @var array Map of HTTP methods -> actions
+	 */
+	protected $_action_map = array
+	(
+		Http_Request::GET     => 'get',
+		Http_Request::OPTIONS => 'options'
+	);
 
 	protected function _scope()
 	{
-		return 'users';
+		return null;
 	}
 
 	protected function _is_auth_required()
 	{
-		return false;
+		return FALSE;
 	}
 
-	
 	/**
-	 * Validate user by email
+	 * Retrieve a basic information about the API
 	 *
-	 * GET /api/users/validate
+	 * GET /api
 	 *
 	 * @return void
 	 */
-	public function action_index()
+	public function action_get_index()
 	{
 		$email = $this->request->param('email');
 		return $query = DB::select()->from('users')
