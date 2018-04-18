c1f01bb1627cdf47ff8648c50ac1175a673a6734
Yohan Florez <yflorezr@gmil.com>
2018-04-18 18:03:42 -0500

    Query Users

--- application/classes/Controller/Api/Validation.php
+++ application/classes/Controller/Api/Validation.php
@@ -23,7 +23,7 @@ class Controller_Api_Validation extends Ushahidi_Rest {
 	 *
 	 * @return void
 	 */
-	public function action_get_validate()
+	public function action_validate()
 	{
 		$email = $this->request->param('email');
 		return $query = DB::select()->from('users')
