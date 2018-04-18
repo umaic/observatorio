b43420c27140a1154a26df62d5a66b6078b2ee25
Yohan Florez <yflorezr@gmil.com>
2018-04-18 18:27:45 -0500

    Query Users

--- application/classes/Controller/Api/Validation.php
+++ application/classes/Controller/Api/Validation.php
@@ -15,6 +15,11 @@ class Controller_Api_Validation extends Ushahidi_Rest {
 		Http_Request::GET     => 'get',
 	);
 
+	protected function _is_auth_required()
+	{
+		return FALSE;
+	}
+
 	protected function _scope()
 	{
 		return 'users';
