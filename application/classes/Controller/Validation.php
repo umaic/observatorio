bb124497da28c9a2c27098a6a2eac9992c22694c
Yohan Florez <yflorezr@gmil.com>
2018-04-19 02:06:21 -0500

    Query Users

--- application/classes/Controller/Validation.php
+++ application/classes/Controller/Validation.php
@@ -9,7 +9,7 @@
  * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
  */
 
-class Controller_Api_Validation extends Ushahidi_Rest {
+class Controller_Validation extends Ushahidi_Rest {
 
 	/**
 	 * @var array Map of HTTP methods -> actions
@@ -23,11 +23,6 @@ class Controller_Api_Validation extends Ushahidi_Rest {
 	protected function _scope()
 	{
 		return null;
-	}
-
-	protected function _is_auth_required()
-	{
-		return FALSE;
 	}
 
 	/**
--- application/routes/default.php
+++ application/routes/default.php
@@ -333,7 +333,6 @@ Route::set('oauth', 'oauth(/<action>)',
 Route::set('config-api-2', $apiBase . 'validate(/<email>)')
 	->defaults(array(
 		'action'     => 'validate',
-		'directory'  => 'Api',
 		'controller' => 'Validation',
 	));
 
