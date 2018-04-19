a1b85d15ae48b7d6291170c36394f87e424ab0a7
Yohan Florez <yflorezr@gmil.com>
2018-04-19 04:08:00 -0500

    Query Users

--- application/classes/Controller/Validation.php
+++ application/classes/Controller/Validation.php
@@ -26,6 +26,7 @@ class Controller_Validation extends Controller {
 		$query = DB::select()->from('users')
 		->where('users.email', '=', $email);
 		$result = $query->execute();
+		$this->request->headers('Content-Type', 'application/json');
 		$this->response->headers('Access-Control-Allow-Origin', '*');
 		$this->response->send_headers();
 		$this->response->body($result->count());
