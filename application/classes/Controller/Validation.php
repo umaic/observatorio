1cd2014176b38646a7565844976388649b1d1bbe
Yohan Florez <yflorezr@gmil.com>
2018-04-19 05:15:54 -0500

    Bug Fix

--- application/classes/Controller/Validation.php
+++ application/classes/Controller/Validation.php
@@ -28,7 +28,6 @@ class Controller_Validation extends Controller {
 		$result = $query->execute();
 		$this->response->headers('Access-Control-Allow-Origin', '*');
 		$this->response->headers('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-		$this->response->headers('Content-Type', 'application/json');
 		$this->response->headers("Access-Control-Allow-Headers", '*');
 		$this->response->body($email);
 	}
