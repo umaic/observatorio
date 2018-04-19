8afc720fc1865df8735a6a299033e801ca4bb38f
Juan Saudade <saudadista@gmail.com>
2018-04-19 15:41:29 -0500

    dump

--- src/Factory/UsecaseFactory.php
+++ src/Factory/UsecaseFactory.php
@@ -93,7 +93,7 @@ class UsecaseFactory
 	 */
 	public function get($resource, $action)
 	{
-		var_dump($resource);
+		echo $resource;
 		if (isset($this->map[$resource][$action])) {
 			$factory = $this->map[$resource][$action];
 		} elseif (isset($this->actions[$action])) {
