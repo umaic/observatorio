741209749bc1e86dadd790487d7bc32401220a72
Yohan Florez <yflorezr@gmil.com>
2018-04-26 12:41:47 -0500

    Reports

--- application/classes/Controller/Reports.php
+++ application/classes/Controller/Reports.php
@@ -26,32 +26,12 @@ class Controller_Reports extends Controller {
 		$query = DB::select()->from('posts')
 		->join(array('forms', 'form'))
 		->on('posts.form_id', '=', 'form.id')
-		->where('posts.post_date', '>=', $weekago)
-		->orderBy('posts.post_date');
+		->where('posts.post_date', '>=', $weekago);
 		$result = $query->execute()->as_array();
-		$totals = [];
-		$last_date = null;
-		$count = 0;
-		$data = [];
-		foreach($result as $r){
-			array_push($data, 'Hola...');
-			//$this->response->body('Hola...');
-			/*if($last_date == null)
-				$last_date = $r['post_date'];
-			if($last_date == $r['post_date']){
-				$count++;
-			}else{
-				array_push($totals, [$last_date => $count]);
-				$count = 1;
-				$last_date = $r['post_date'];
-			}*/
-		}
-		/*$data = array();
-		array_push($data, ['totals' => $totals]);*/
 		$this->response->headers('Access-Control-Allow-Origin', '*');
 		$this->response->headers('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
 		$this->response->headers("Access-Control-Allow-Headers", '*');
-		//$this->response->headers('Content-Type', 'application/json; charset=utf-8');
-		$this->response->body('Hola....');
+		$this->response->headers('Content-Type', 'application/json; charset=utf-8');
+		$this->response->body(json_encode($result));
 	}
 }
