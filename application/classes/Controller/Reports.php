69dce41dbbeca65629a40db7da997c223b768ade
Yohan Florez <yflorezr@gmil.com>
2018-04-26 12:40:39 -0500

    Reports

--- application/classes/Controller/Reports.php
+++ application/classes/Controller/Reports.php
@@ -52,6 +52,6 @@ class Controller_Reports extends Controller {
 		$this->response->headers('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
 		$this->response->headers("Access-Control-Allow-Headers", '*');
 		//$this->response->headers('Content-Type', 'application/json; charset=utf-8');
-		$this->response->body(json_encode($data));
+		$this->response->body('Hola....');
 	}
 }
