25c13c5cd2e563048f28b4b021bbfee00300a1ef
Yohan Florez <yflorezr@gmil.com>
2018-04-26 12:15:32 -0500

    Reports

--- application/classes/Controller/Reports.php
+++ application/classes/Controller/Reports.php
@@ -27,10 +27,10 @@ class Controller_Reports extends Controller {
 		->join(array('forms', 'form'))
 		->on('posts.form_id', '=', 'form.id')
 		->where('posts.post_date', '>=', $weekago);
-		$result = $query->execute()->as_array();;
+		$result = $query->execute()->as_array();
 		$this->response->headers('Access-Control-Allow-Origin', '*');
 		$this->response->headers('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
 		$this->response->headers("Access-Control-Allow-Headers", '*');
-		$this->response->body($result);
+		$this->response->body(json_encode($result));
 	}
 }
